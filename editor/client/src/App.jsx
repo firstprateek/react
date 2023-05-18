f39e54c6fc431800eee23671b51c31c6ec20ef64
Prateek Joshi <padi@MacBook-Pro-2.local>
2023-05-18 19:25:35 -0400

    doc like collaborative editor

--- editor/client/src/App.jsx
+++ editor/client/src/App.jsx
@@ -1,3 +1,19 @@
+import TextEditor from "./TextEditor";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom'
+import { v4 as uuidV4 } from 'uuid';
+
 export default function App() {
-  return "Hi";
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to={`/documents/${uuidV4()}`} />} />
+        <Route path="/documents/:id" element={<TextEditor />} />
+      </Routes>
+    </Router>
+  )
 }
