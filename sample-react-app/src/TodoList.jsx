4abaa5d6d21d9ad8d221354d6dbafc24730477ca
Prateek Joshi <padi@MacBook-Pro-2.local>
2023-05-18 15:24:11 -0400

    todo list update

--- sample-react-app/src/TodoList.jsx
+++ sample-react-app/src/TodoList.jsx
@@ -1,6 +1,5 @@
 import TodoItem from './TodoItem';
 
 export default function TodoList({ todos, toggleTodo, deleteTodo }) {
-    {todos.length === 0 && "No Todos"}
     return todos.map(todo => <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />);
 }
